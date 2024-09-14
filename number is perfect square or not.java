a64451055acdf95352211137a7570a3da981fdde
Keerthana <keerthanaravi1133@gmail.com>
2024-09-14 12:45:06 +0530

    number is perfect square or not
    whether given number is perfect square or not

---
+++ number is perfect square or not.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+    int i;
+    boolean res=false;
+    for(i=1;i*i<=num;i++)
+    {
+      if((num%i==0) && (num/i==i)){
+        res=true;
+      }
+    }
+      if(num==i*i){
+        System.out.println("Perfect Square");
+      }
+      else{
+      System.out.println("Not");
+    }
+    
+    
+  
+}
+}
